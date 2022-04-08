b6b288e3bb9ea9869e363f8196fd0c1e9a67de2e
Ratul Samanta <ratul.developer@gmail.com>
2022-04-08 19:55:39 +0530

    remove php info

--- index.php
+++ index.php
@@ -12,6 +12,5 @@
  * @since         0.10.0
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
-echo phpinfo();
-exit();
+
 require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
